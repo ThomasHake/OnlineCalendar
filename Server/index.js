5d195052ee3d7c572b3e9968f9e0f48199e71e79
ThomasHake <thomaskhake@gmail.com>
2022-08-12 17:35:50 -0700

    add, remove, update

--- Server/index.js
+++ Server/index.js
@@ -2,19 +2,14 @@ const express = require('express');
 const server = express();				
 const path = require('path');
 const Datastore = require('nedb')
-//const fs = require('fs');
-//const cors = require('cors');    	
-
-//server.use(cors({origin:'*'}))
 
 server.use(express.static(path.join(__dirname, '..', 'Client')));
 server.use(express.json({limit: '1mb'}));      
 
+
 const db = {};
 db.events = new Datastore('events.db');
 db.events.loadDatabase();
-
-
 
 //send all events
 server.get('/calendarjs/events/all', (req, res) => {
@@ -28,6 +23,36 @@ server.get('/calendarjs/events/all', (req, res) => {
 		res.json(events);
 	});
 });
+
+
+//post test
+server.post('/calendarjs/events', (req, res) => {
+	console.log(req.body);
+	db.events.insert(req.body);
+	res.end();
+});
+//delete test
+server.delete('/calendarjs/events', (req, res) => {
+	console.log(req.body);
+	db.events.remove({id:req.body.id}, {}, (err, numRemoved) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+	res.end();
+});
+//update test
+server.put('/calendarjs/events', (req, res) => {
+	console.log(req.body);
+	db.events.update({id: req.body.id}, {$set: req.body}, {}, (err, numUpdated) => {
+		if (err){
+			console.log(err);
+		}
+	});
+	res.end();
+});
+
+
 
 
 const port = process.env.PORT || 3000;
